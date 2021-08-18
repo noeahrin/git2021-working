ea026dcdd529191a1b651db37bbe868526b451e6
Jihwan Seo <noeahrin@gmail.com>
2021-08-18 21:25:23 +0900

    20210818

--- 4-react/myworkspace/src/index.tsx
+++ 4-react/myworkspace/src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+// 모듈: 부분 코드
+// App.tsx 모듈을 가져오고(읽어와서) App이름으로 선언함
+// export default로 내보낸 객체가 App이름으로 선언됨
+// 가져온 App 함수를 이름을 꼭 App 말고 다른걸로 바꿔도 됨 근데 일반적으로 맞춰서 씀
+import App from './App';  // App.tsx      
+
 import reportWebVitals from './reportWebVitals';
 
+// id가 root인 요소에 App 컴포넌트를 렌더링함
+// 컴포넌트(component): 부분 UI, 기능 포함
+// 템플릿(template): 부분 UI, UI 구좀나 가짐
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> {/* App 컴포넌트 */}
   </React.StrictMode>,
   document.getElementById('root')
 );
